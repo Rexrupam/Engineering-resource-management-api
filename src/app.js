715ed3aa0a595ba218934ab23a1a28d8f38be68a
rupam <rexrupam70@gmail.com>
2025-08-01 16:01:08 +0530

    Initial file structure and database connection

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import HealthCheckRouter from "./router/healthCheck.route.js"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+const app = express()
+
+app.use(cors({
+    origin: process.env.cors_origin,
+    credentials: true
+}))
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use('/', HealthCheckRouter)
+export { app }; 
